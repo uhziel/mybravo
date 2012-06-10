129908f412383c59b717036a067a0f753dfca847
Lei Zhu <uhziel@gmail.com>
2012-06-10 16:32:57 +0800

    handle packet login.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from twisted.internet import reactor
 from twisted.internet.protocol import Factory, Protocol
 
 from construct import LengthValueAdapter, StringAdapter, Sequence
-from construct import UBInt16
+from construct import UBInt16, SBInt32, SBInt8, UBInt8
 from construct import Struct, MetaField
 
 STATE_UNAUTHENTICATED, STATE_CHALLENGED = range(2)
@@ -27,6 +27,17 @@ def BetaString(name):
 
 
 parsers = {
+    0x01: Struct(
+        'login',
+        SBInt32('protocol_version'),
+        BetaString('username'),
+        BetaString('not_used1'),
+        SBInt32('not_used2'),
+        SBInt32('not_used3'),
+        SBInt8('not_used4'),
+        UBInt8('not_used5'),
+        UBInt8('not_used6'),
+    ),
     0x02: Struct(
         'handshake',
         BetaString('username_and_host')
@@ -49,6 +60,17 @@ class BetaProtocol(Protocol):
         else:
             print 'unknown packet (0x{:02X}, {})'.format(packet_id, repr(payload))
 
+    def login(self, container):
+        print 'login: protocol version is {}, username is {}'.format(
+            container.protocol_version,
+            container.username,
+        )
+
+        if container.protocol_version < 29:
+            self.trasnport.write('\xff')
+
+        self.transport.write('\x01' + '\x00' * 8)
+
     def handshake(self, container):
         print 'handshake: username_and host is {}'.format(
             container.username_and_host
@@ -59,6 +81,7 @@ class BetaProtocol(Protocol):
         self.transport.write('\x02' + parsers[0x02].build(container))
 
     handlers = {
+        0x01: login,
         0x02: handshake,
     }
 
